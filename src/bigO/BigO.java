35831ebd512e2784b0c3a6fbe35340bcda45d392
Amruta Kangiri <47620598+AmrutaK28@users.noreply.github.com>
2025-01-03 06:18:35 +0000

    Big O created

--- src/bigO/BigO.java
+++ src/bigO/BigO.java
@@ -2,13 +2,13 @@
 
 // import org.junit.jupiter.api.Test;
 
-public class Main {
+public class BigO {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Big O");
   }
 
   // @Test
   // void addition() {
-  //     assertEquals(2, 1 + 1);
+  // assertEquals(2, 1 + 1);
   // }
 }
